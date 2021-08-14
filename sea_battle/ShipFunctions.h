37d8154fdda81568e048050686071acca795fe2e
cyclik <dimakirimail@gmail.com>
2021-08-14 14:17:40 +0300

    adding new files for Ship functions

---
+++ sea_battle/ShipFunctions.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <string>
+#ifndef FunctionsForShip
+#define FunctionsForShip
+
+
+int ConvertLetToNum(char letter);
+int ConvertNumToNum(char num);
+void MapReset(char sea[10][10]);
+void Show(char sea[10][10]);
+bool CanShipBePlacedHere(int y, int x, char sea[10][10]);
+std::string CanPlaceShip(char sea[10][10], Ship ship);
+void PlaceShip(char sea[10][10], Ship ship);
+Ship GetShipInfo();
+bool ShouldPlaceShips();
+void ShipPlacement(char sea[10][10]);
+#endif 
+
